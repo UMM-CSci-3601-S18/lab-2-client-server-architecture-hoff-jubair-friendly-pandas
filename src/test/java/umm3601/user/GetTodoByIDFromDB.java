1257e1492440803d2e8e3277e757c3df00942855
john9hoff <hoffx247@morris.umn.edu>
2018-02-11 18:05:51 -0600

    Create Todo Test File
    As a clone to the GetUserByIDFromDB file, the GetTodoByIDFromDB test runs and passes.

---
+++ src/test/java/umm3601/user/GetTodoByIDFromDB.java
@@ -0,0 +1,20 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Tests umm3601.user.Database getUser functionality
+ */
+public class GetTodoByIDFromDB {
+
+  @Test
+  public void getStokesClayton() throws IOException {
+    ToDoDatabase db = new ToDoDatabase("src/main/data/todos.json");
+    ToDo todo = db.getToDo("58895985a22c04e761776d54");
+    assertEquals("Incorrect name", "Blanche", todo.owner);
+  }
+}
